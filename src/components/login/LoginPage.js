596ac710d8750ad79b6b8e4f145412a7df753e5b
Maxime Antoine <maxime.antoine2@gmail.com>
2018-01-07 15:46:53 +0800

    Added link

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './login-page.css';
 
 class LoginPage extends React.Component {
@@ -16,6 +17,8 @@ class LoginPage extends React.Component {
                     </div>
                     <button type="button" className="btn btn-primary">Login</button>
                 </form>
+                <br />
+                <p>No account yet ? <Link to="/register">Register !</Link></p>
             </div>
         );
     }
